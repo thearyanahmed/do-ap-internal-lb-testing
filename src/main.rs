d1276f27e03855359a5cf993b9098af29e08b018
Aryan <paryan@digitalocean.com>
2025-04-10 11:17:36 +0100

    Bind to the world

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(data.clone())
             .route("/", web::get().to(handle_request))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
