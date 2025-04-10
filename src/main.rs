b403f4940ce7811bf0d0db196de8ede34805010d
Aryan <paryan@digitalocean.com>
2025-04-10 10:54:52 +0100

    Add uuid to identify the server

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 use actix_web::{web, App, HttpServer, Responder};
 use std::sync::Mutex;
+use uuid::Uuid;
 
 struct AppState {
     request_count: Mutex<u32>,
+    instance_uuid: String,
 }
 
 async fn handle_request(data: web::Data<AppState>) -> impl Responder {
+    let instance_uuid = &data.instance_uuid;
+
     let mut count = data.request_count.lock().unwrap();
     *count += 1;
-    format!("This server has handled {} requests.", count)
+
+    format!("[{}] server has handled {} requests.", instance_uuid, count)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let id = Uuid::new_v4();
+
     let data = web::Data::new(AppState {
         request_count: Mutex::new(0),
+        instance_uuid: id.to_string(),
     });
+
+    println!("starting server with UUID: {}", id);
 
     HttpServer::new(move || {
         App::new()
